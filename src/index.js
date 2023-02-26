aceb3de748858027220b3361d55275f3bb9ba8af
Bogdan <bogdanberezka@gmail.com>
2023-02-26 19:47:56 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
+        <BrowserRouter basename="practice-with-mentor-04">
           <App />
         </BrowserRouter>
       </PersistGate>
